002f157a1439f1113698177dcf65b0bd8b740ab5
Ben Kazemi <ebaykazemi@googlemail.com>
2015-09-02 19:12:10 +0100

    removed licence in non root folder and main.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
-#include "main.h"
+#include "cpu.h"
+#include <avr/io.h>
+#include <stdio.h>
+#include "serial.h"
+#include "global.h"
+#include "init.h"
+#include "control.h"
 
+#define MAIN_PACKET_END_BYTE           0xFF
 
+uint8_t sensor = 0; 
 
 int main(void) {
 	initStartup();
