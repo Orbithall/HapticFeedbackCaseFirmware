1c66ca8d6352be7c6c3b70360b7f03ad40f1e662
Ben Kazemi <ebaykazemi@googlemail.com>
2015-09-06 21:54:33 +0100

    added teh licence preamble

--- src/global.h
+++ src/global.h
@@ -1,6 +1,7 @@
 /*
-   Haptic Feedback Case - Firmware
-   Copyright (C) 2015: Ben Kazemi, ben.kazemi@gmail.com
+	Haptic Feedback Case 
+	Copyright (C) 2015:
+         Ben Kazemi, ebaykazemi@googlemail.com
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -16,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+
 
 #ifndef GLOBAL_H
 #define GLOBAL_H
